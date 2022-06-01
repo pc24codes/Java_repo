455078a4321ea07dc9d04a844e735806ed036aef
parasuramc <parasuramreddy2000@gmail.com>
2022-06-01 23:36:19 +0530

    second commit for banktester

--- Bank_tester.java
+++ Bank_tester.java
@@ -6,9 +6,9 @@ public class Bank_tester {
 		// TODO Auto-generated method stub
 
 		Bank_main b1 = new Bank_main();
-		b1.BankName = "SBI";
+		b1.BankName = "HDFC";
 		b1.Area= "hyderabad";
-		b1.phonenumber= 123456789;
+		b1.phonenumber= 884574452;
 		b1.getDetails();
 	}
 
